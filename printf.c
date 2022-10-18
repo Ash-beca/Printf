660dde392ad2f1f7b60b99bd40c15d996d2a6ec5
Jemima Okouya <ashbeca16@gmail.com>
2022-10-18 02:53:30 -0700

    printf

--- printf.c
+++ printf.c
@@ -4,6 +4,43 @@
 #include <stdlib.h>
 
 /**
+ * get_print - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
+ */
+
+int (*get_print(const char c))(va_list)
+{
+	int i = 0;
+
+	flags_t ft[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
+		{"R", print_rot13},
+		{"%", print_percent}
+	};
+	while (i < 14)
+	{
+		if (c == fp[i].c[0])
+		{
+			return (ft[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
  * _printf - produces output according to a format
  * @format - valid format specifier
  * @get_print - functions that will be called during conversion
