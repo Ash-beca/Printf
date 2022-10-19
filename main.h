c3f039c64909ecb9776d344e38258a5dacf69640
Jemima Okouya <ashbeca16@gmail.com>
2022-10-19 02:23:27 -0700

    updated main.h

--- main.h
+++ main.h
@@ -6,6 +6,20 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_int(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_lower(va_list ap);
+int print_hexa_upper(va_list ap);
+int print_unsigned(va_list ap);
+int print_str_unprintable(va_list ap);
+int print_str_reverse(va_list ap);
+int print_ptr(va_list ap);
+int print_rot13(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+
 
  /**
   * struct specifier - struct to choose the right function depending
@@ -20,3 +34,4 @@ typedef struct specifier
 		char c;
 		int (*f)(va_list);
 } spec;
+#endif
